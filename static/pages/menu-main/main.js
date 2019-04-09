e7b31710345043b2a771ecd1c3e40531fe35ddd1
LittleBigKiller <35974496+LittleBigKiller@users.noreply.github.com>
2019-04-09 20:41:50 +0200

    DisplayTests added, CSS fixes

--- static/pages/menu-main/main.js
+++ static/pages/menu-main/main.js
@@ -1,43 +1,89 @@
 $(document).ready(() => {
-    console.log("document ready");
+    console.log('document ready');
 
     //#region menu listeners
-    $("#bMain0").click(() => {
-        window.location = "/"
+    $('#bMain0').click(e => {
+        if (!e.target.className.includes('disabled')) {
+            window.location = '/static/pages/menu-main/main.html'
+        }
     })
 
-    $("#bMain1").click(() => {
-        window.location = "/"
+    $('#bMain1').click(e => {
+        if (!e.target.className.includes('disabled')) {
+            window.location = '/static/pages/menu-main/main.html'
+        }
     })
 
-    $("#bMain2").click(() => {
-        window.location = "/"
+    $('#bMain2').click(e => {
+        if (!e.target.className.includes('disabled')) {
+            // window.location = '/static/pages/menu-main/main.html'
+            DisplayTests(testList)
+        }
     })
 
-    $("#bMain3").click(() => {
-        window.location = "/"
+    $('#bMain3').click(e => {
+        if (!e.target.className.includes('disabled')) {
+            window.location = '/static/pages/menu-main/main.html'
+        }
     })
 
-    $("#bMain4").click(() => {
-        window.location = "/"
-    })
-
-    $("#btDatabaseTest").click(() => {
-        window.location = "/static/pages/test/database_test/main.html"
-    })
-
-    $("#test").click(() => {
-        setTimeout(() => {
-            let status = document.getElementById("test").open
-            if (status) {
-                $("#btTest").css("background-color", "#aaaaaa")
-            }
-            else {
-                $("#btTest").css("background-color", "")
-            }
-        })
+    $('#bMain4').click(e => {
+        if (!e.target.className.includes('disabled')) {
+            window.location = '/static/pages/menu-main/main.html'
+        }
     })
     //#endregion
 
+    let testList = [
+        {
+            name: 'test0',
+            path: '/static/pages/menu-main/main.html'
+        },
+        {
+            name: 'test1',
+            path: '/static/pages/menu-main/main.html'
+        },
+        {
+            name: 'test2',
+            path: '/static/pages/menu-main/main.html'
+        },
+        {
+            name: 'test3',
+            path: '/static/pages/menu-main/main.html'
+        },
+        {
+            name: 'test4',
+            path: '/static/pages/menu-main/main.html'
+        },
+    ]
 
+    function DisplayTests(list) {
+        if (list.length > 6) console.warn('DisplayTests might not display all options properly (amount > 6)')
+
+        let overlay = $('#overlay')
+        if (overlay.css('display') == 'none') {
+            overlay.removeAttr('style')
+
+            let winContainer = $('#container')
+            winContainer.html('')
+
+            $('#header').html('Tests')
+
+            for (let i = 0; i < list.length; i++) {
+                let bOption = $('<div>')
+                bOption.addClass('window-button')
+                bOption.html(list[i].name)
+                winContainer.append(bOption)
+                bOption.click(() => {
+                    window.location = list[i].path
+                })
+            }
+
+            $('#win-bBack').click(() => {
+                overlay.css('display', 'none')
+            })
+        } else {
+            throw 'DisplayTests was called incorrectly'
+        }
+    }
 })
