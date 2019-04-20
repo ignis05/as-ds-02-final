7be9a5026230343b82c70ce830b494705f1167a7
ignis05 <48099798+ignis05@users.noreply.github.com>
2019-04-20 17:57:48 +0200

    Code refactoring

--- static/pages/lobby/main.js
+++ static/pages/lobby/main.js
@@ -1,8 +1,46 @@
-// initialization:
-var socket = io(`/lobby`) // connect to socket instance `/lobby` (fyi actual path used by socket is http://host:port/socket.io/lobby - so this won't cause any errors)
+// #region socket.io
+var socket = io(`/lobby`)
 
+socket.roomMembers = []
 
-//          >>>> bunch of triggers that are fired remotely by server or other clients:
+socket.createRoom = function (roomName) {
+    socket.emit('room_create', roomName)
+}
+socket.joinRoom = function (roomName) {
+    socket.emit('room_join', roomName)
+}
+socket.leaveRoom = function () {
+    socket.emit('room_leave')
+}
+socket.send = function (msg) {
+    socket.emit('send', msg)
+}
+socket.setName = function (nickname) {
+    socket.emit('setName', nickname)
+}
+socket.getRooms = function () {
+    return new Promise(resolve => {
+        socket.emit('getRooms', res => {
+            resolve(res)
+        })
+    })
+}
+socket.getClients = function () {
+    return new Promise(resolve => {
+        socket.emit('getClients', res => {
+            resolve(res)
+        })
+    })
+}
+socket.getMyRooms = function () {
+    return new Promise(resolve => {
+        socket.emit('get_my_rooms', res => {
+            resolve(res)
+        })
+    })
+}
+
+//      #region socket events
 
 // triggers when someone tries to connect using token that is alredy in use - client should be immediately redirected to main page
 socket.on('error_token', () => {
@@ -39,75 +77,17 @@ socket.on('rooms_updated', () => {
     updateRoomMembers()
 })
 
-// #region socket functions (they are described below) - collapse code for convenience
-function socket_createRoom(roomName) {
-    socket.emit('room_create', roomName)
-}
-function socket_joinRoom(roomName) {
-    socket.emit('room_join', roomName)
-}
-function socket_leaveRoom() {
-    socket.emit('room_leave')
-}
-function socket_send(msg) {
-    socket.emit('send', msg)
-}
-function socket_setName(nickname) {
-    socket.emit('setName', nickname)
-}
-function socket_getRooms() {
-    return new Promise(resolve => {
-        socket.emit('getRooms', res => {
-            resolve(res)
-        })
-    })
-}
-function socket_getClients() {
-    return new Promise(resolve => {
-        socket.emit('getClients', res => {
-            resolve(res)
-        })
-    })
-}
-function socket_get_my_rooms() {
-    return new Promise(resolve => {
-        socket.emit('get_my_rooms', res => {
-            resolve(res)
-        })
-    })
-}
-// #endregion socket functions
+//      #endregion socket events
+
+// #endregion socket.io
 
 
-// #region >>> functions descriptions : <<<
-
-// socket_setName(nickname) // - changes client nickname (empty nickname won't cause error - though you might want to prevent that for obvious reasons) 
-// ^- (nicknames will be recognized on disconnect and reconect, as long as server is running)
-// ^-- (in future it might be a good idea to add limit of remembered clients to prevent memory leaks on longer server runs)
-
-// socket_createRoom(roomName) // - creates new room and joins it - REQUIRES CHECK IF ROOM DOESN'T EXIST YET
-// socket_joinRoom(roomName) // - joins room matching given name (automatically leaves previous room) - REQUIRES CHECK IF ROOM EXIST
-// socket_leaveRoom() // - leaves current room (if client isn't currently in any room nothing happens)
-
-// socket_send(msg) // - triggers 'chat' event for all clients in current room (>including self< - if you want that changed, ask) (if not in any room nothing will happen)
-
-// await socket_getClients() - returns client array (all clients (even ones not assigned to any room) with their names and tokens)
-// await socket_getRooms() - returns room array (room name, room admin , clients inside room)
-// await get_my_rooms() - returns room object with all rooms socket is in
-
-//socket.emit('carryRoomName', roomName) - joins room on next connect - used to carry name from '/' to '/lobby'
-
-// #endregion >>> functions descriptions : <<<
-
-// function that updates room members list
-
-var roomMembers = []
 
 async function updateRoomMembers() { // placeholder function triggered by 'rooms_updated' event
-    let rooms = await socket_getRooms()
+    let rooms = await socket.getRooms()
     let room = rooms.find(room => room.clients.find(client => client.id == socket.id))
     let display = ""
-    roomMembers = room.clients
+    socket.roomMembers = room.clients
     for (let client of room.clients.map(client => client.name)) {
         display += client + '<br>'
     }
@@ -116,7 +96,7 @@ async function updateRoomMembers() { // placeholder function triggered by 'rooms
 }
 
 async function updateChat(msg) { // placeholder function triggered by 'chat' event
-    let author = roomMembers.find(client => client.id == msg.author)
+    let author = socket.roomMembers.find(client => client.id == msg.author)
 
     let chat = $('#socket-chat-display').html()
     chat += `<div style="display: inline; color: #FF0000; text-shadow: 2px 0px 1px #000000;">${author.name}</div>: ${msg.content} <br>`
@@ -126,7 +106,7 @@ async function updateChat(msg) { // placeholder function triggered by 'chat' eve
 $(document).ready(async () => {
     // block of code that cheks if user succesfully joined a room
     // if not (for example he refreshed page or sth) he should be redirected to main page
-    let myRooms = await socket_get_my_rooms()
+    let myRooms = await socket.getMyRooms()
     if (Object.values(myRooms).length < 2) {
         window.location = '/'
     }
@@ -159,11 +139,14 @@ $(document).ready(async () => {
                     author: socket.id, // id of current connection - will always be unique
                     content: $('#socket-chat-input').val()
                 }
-                socket_send(msg) // trigger event 'chat' for every user in room (including self) and pass msg obj as data
+                socket.send(msg) // trigger event 'chat' for every user in room (including self) and pass msg obj as data
                 $('#socket-chat-input').val('') // Clear message field to prevent accidental spam
             }
         }
     })
+
+    // focus chat input on click
+    $('#region-chat').click(() => document.getElementById('socket-chat-input').focus())
 })
 
 
