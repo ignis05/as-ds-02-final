1406c5d8976205b399556b03bd75d95728579b3b
LittleBigKiller <35974496+LittleBigKiller@users.noreply.github.com>
2019-05-08 11:30:12 +0200

    Proof of concept usage of BlockTypes.js

---
+++ static/js/BlockTypes.js
@@ -0,0 +1,20 @@
+const MASTER_BlockTypes = {
+    dirt: {
+        editor: {
+            color: '#338833',
+        },
+        game: {
+            color: 0x888888,
+            walkable: true,
+        }
+    },
+    rock: {
+        editor: {
+            color: '#888888',
+        },
+        game: {
+            color: 0x888888,
+            walkable: false,
+        }
+    },
+}
