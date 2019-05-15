9d663c6ad8ef4de76a3d5c6e2efc7a3fe2362013
LittleBigKiller <35974496+LittleBigKiller@users.noreply.github.com>
2019-05-16 00:34:09 +0200

    Added UI template to Game, Added minimap

---
+++ static/pages/game/ui.js
@@ -0,0 +1,20 @@
+class UI {
+    constructor() {
+        // Nothing here for now
+    }
+
+    MinimapCalc(levelPack) {
+        let canvas = $('#minimap-canvas')
+            .attr('width', parseInt(levelPack.size) * 8)
+            .attr('height', parseInt(levelPack.size) * 8)
+    
+        let ctx = canvas[0].getContext('2d')
+    
+        ctx.scale(8, 8)
+    
+        for (let i in levelPack.level) {
+            ctx.fillStyle = MASTER_BlockTypes[levelPack.level[i].type].editor.color
+            ctx.fillRect(parseInt(levelPack.level[i].x), parseInt(levelPack.level[i].z), parseInt(levelPack.level[i].x) + 1, parseInt(levelPack.level[i].z) + 1)
+        }
+    }
+}
