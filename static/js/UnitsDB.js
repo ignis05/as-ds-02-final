17804074feccff5de5c2146fca66242ae7e4ed26
ignis05 <48099798+ignis05@users.noreply.github.com>
2019-06-05 18:19:58 +0200

    Implemented OBJ Loader in game

--- static/js/UnitsDB.js
+++ static/js/UnitsDB.js
@@ -1,27 +1,27 @@
 // database for unit types
 const MASTER_Units = {
-    spider: {
-        modelURL: '/static/res/models/spider/spider.fbx',
-        scale: 0.5,
-        posY: 0,
-        stats: {
-            mobility: 5,
-            health: 10,
-            damage: 5,
-            range: 1
-        },
-    },
-    raptoid: {
-        modelURL: '/static/res/models/raptoid/scene.gltf',
-        scale: 1,
-        posY: 0,
-        stats: {
-            mobility: 5,
-            health: 10,
-            damage: 5,
-            range: 1
-        },
-    },
+    // spider: {
+    //     modelURL: '/static/res/models/spider/spider.fbx',
+    //     scale: 0.5,
+    //     posY: 0,
+    //     stats: {
+    //         mobility: 5,
+    //         health: 10,
+    //         damage: 5,
+    //         range: 1
+    //     },
+    // },
+    // raptoid: {
+    //     modelURL: '/static/res/models/raptoid/scene.gltf',
+    //     scale: 1,
+    //     posY: 0,
+    //     stats: {
+    //         mobility: 5,
+    //         health: 10,
+    //         damage: 5,
+    //         range: 1
+    //     },
+    // },
     overlord: {
         modelURL: '/static/res/models/overlord/model.fbx',
         scale: 0.2,
